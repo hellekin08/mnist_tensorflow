80276c759db48439a63447cd29120069256c6d8b
Hellekin87 <30690557+Hellekin87@users.noreply.github.com>
2017-08-03 12:47:00 +0200

    Update mnist.py

--- mnist.py
+++ mnist.py
@@ -30,51 +30,25 @@ def create_layer(init_nodes,n_nodes_this_layer,data):
 	layer = tf.nn.relu(layer)	
 	return layer
 
-#trainieren des models input data = x
 def train_neural_network(x):
-	#Die schötzung ist das Ergebnis unseres models als Array z.b. (1,0,0,0,0,0,0,0,0,0)
+	
 	prediction = neural_network_model(x)
-	# cost = lost softmax_cross_entropy_with_logits ist die cost-function
-	# berechnet die differenz zwischen prediction und einem bekannten label!!!!
-	# one_hot definiert dabei den output
 	cost = tf.reduce_mean( tf.nn.softmax_cross_entropy_with_logits(logits = prediction, labels=y) )
-
-	#minimieren des cost mit optimizier
-	#AdamOptimizer nutzt gradient descent ??
-	#optional learning_rate = 0.001
 	optimizer = tf.train.AdamOptimizer().minimize(cost)
-
-	#Anzahl der Epochen festlegen
-	#durchläuft 10mal feedforward + backprop
 	hm_epochs = 10
 
-	#starten des Session
 	with tf.Session() as sess:
 		sess.run(tf.initialize_all_variables())
-
-		# durchlaufen aller epochen
 		for epoch in range(hm_epochs):
 			epoch_loss = 0
-			# total number of samples / batch_sizes sagt uns wie of wir 
-			# durch eine epoche iterieren müssen
 			for _ in range(int(mnist.train.num_examples/batch_size)):
-				#lädt die daten des mnist-datensatzes als 100ter Stapel
-				#die funktion muss für eigene daten selber geschrieben werden!
 				epoch_x, epoch_y = mnist.train.next_batch(batch_size)
-				#Starten des Session mit dem Optimizer und der cost
-				#feed_dict ist die übergabe der Daten 
-				#nun weiß tf das es die weights und biases anpassen muss 
 				_, c = sess.run([optimizer,cost],feed_dict={x: epoch_x, y: epoch_y})
 				epoch_loss += c
 
 			print("Epoch",epoch,"completed out of",hm_epochs,"loss:",epoch_loss)
-
-		#argmax???????? returns index of maximum value in den Arrays!! prediction und y
-		#überprüpft ob der wert des maximalen index gleich ist!
 		correct = tf.equal(tf.argmax(prediction,1), tf.argmax(y,1))
-		#Changes the variable to float
 		accuracy = tf.reduce_mean(tf.cast(correct,'float'))
-		#überprüfen aller Accurancy der bilder zu den labels
 		print('Accuracy:',accuracy.eval({x:mnist.test.images, y:mnist.test.labels}))
 
 train_neural_network(x)
